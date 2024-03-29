f0b9a3ef64b7972acd9e2b7291e5b8d666b8d576
Maxym Palchei <maksimpalchey@gmail.com>
2024-03-29 12:12:48 +0200

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+
+public class PrimaryController {
+    /*@FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }*/
+}
